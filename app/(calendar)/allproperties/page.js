09a700fbd609fd24dc6795d20d2b8faa5bb89c86
Hadi Tedi <haditedi@gmail.com>
2024-06-21 08:11:29 +0700

    updated notes

--- app/(calendar)/allproperties/page.js
+++ app/(calendar)/allproperties/page.js
@@ -4,10 +4,10 @@ export default function Allproperties() {
       <div className="w-[250px] h-[350px] md:w-[550px] p-2">
         <h1>Bowden House</h1>
         <p>Flat 43 Bowden House on the 5th Floor. 9 Palmer Road SW11 4FW</p>
-        <b style={{ color: "red", marginBottom: "15px" }}>
+        {/* <b style={{ color: "red", marginBottom: "15px" }}>
           <p>NOTE:</p>
           <p>20 June - No Arrival</p>
-        </b>
+        </b> */}
         <iframe
           src="https://calendar.google.com/calendar/embed?src=dv6h28r9avoe0i2246p78sufe9e5jqcr%40import.calendar.google.com&ctz=UTC"
           width="800"
@@ -30,9 +30,6 @@ export default function Allproperties() {
         <p>20 Prince square Flat 3B. W2 4NP</p>
         <b style={{ color: "red", marginBottom: "15px" }}>
           <p>NOTE:</p>
-          <p>Arrival 12 June until departure 19 June.</p>
-          <p>No arrival 19 June.</p>
-          <p>Arrival 20 June until departure 22 June.</p>
           <p>5 July - no arrival</p>
         </b>{" "}
         <br />
@@ -46,10 +43,10 @@ export default function Allproperties() {
       <div className="w-[250px] h-[350px] md:w-[550px] pt-4 mt-4">
         <h1 style={{ marginTop: "50px" }}>Sky Garden</h1>
         <p>Flat 1008, 155 Wandsworth Road, London SW8 2FW</p>
-        <b style={{ color: "red", marginBottom: "15px" }}>
+        {/* <b style={{ color: "red", marginBottom: "15px" }}>
           <p>NOTE:</p>
           <p>24 June - no arrival</p>
-        </b>
+        </b> */}
         <iframe
           src="https://calendar.google.com/calendar/embed?src=pfnoec62930muadpkjs5d96hsnfaajbg%40import.calendar.google.com&ctz=UTC"
           width="800"
@@ -60,10 +57,10 @@ export default function Allproperties() {
       <div className="w-[250px] h-[350px] md:w-[550px] pt-4 mt-4">
         <h1 style={{ marginTop: "50px" }}>Grafton Way</h1>
         <p>Flat 111, Grafton Way 30-40, London WC1E 6DY</p>
-        <b style={{ color: "red", marginBottom: "15px" }}>
+        {/* <b style={{ color: "red", marginBottom: "15px" }}>
           <p>NOTE:</p>
           <p>16 June - deep cleaning + setup (TBC)</p>
-        </b>
+        </b> */}
         <iframe
           src="https://calendar.google.com/calendar/embed?src=kemg78v2m95n861fq11cntdl3qngu2ks%40import.calendar.google.com&ctz=UTC"
           width="800"
