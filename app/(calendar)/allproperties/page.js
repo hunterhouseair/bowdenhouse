1d1ab59ada62eaa793ce78db496b4c715c823d11
Hadi Tedi <haditedi@gmail.com>
2024-08-29 17:30:01 +0100

    removed notes

--- app/(calendar)/allproperties/page.js
+++ app/(calendar)/allproperties/page.js
@@ -17,10 +17,10 @@ export default function Allproperties() {
       <div className="w-[250px] h-[350px] md:w-[550px] pt-4 mt-4">
         <h1 style={{ marginTop: "50px" }}>Hunter House</h1>
         <p>Flat 8 Hunter House London WC1N 1BE</p>
-        <b style={{ color: "red", marginBottom: "15px" }}>
+        {/* <b style={{ color: "red", marginBottom: "15px" }}>
           <p>NOTE:</p>
           <p>16 August - No Arrival</p>
-        </b>
+        </b> */}
         <iframe
           src="https://calendar.google.com/calendar/embed?src=i2qmd3b458ofocnj6gnij81ku2lturqg%40import.calendar.google.com&ctz=UTC"
           width="800"
@@ -72,10 +72,10 @@ export default function Allproperties() {
       <div className="w-[250px] h-[350px] md:w-[550px] pt-4 mt-4">
         <h1 style={{ marginTop: "50px" }}>Grafton Way</h1>
         <p>Flat 111, Grafton Way 30-40, London WC1E 6DY</p>
-        <b style={{ color: "red", marginBottom: "15px" }}>
+        {/* <b style={{ color: "red", marginBottom: "15px" }}>
           <p>NOTE:</p>
           <p>16 August - No Arrival</p>
-        </b>
+        </b> */}
         <iframe
           src="https://calendar.google.com/calendar/embed?src=kemg78v2m95n861fq11cntdl3qngu2ks%40import.calendar.google.com&ctz=UTC"
           width="800"
