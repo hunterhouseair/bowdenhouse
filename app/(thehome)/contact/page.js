d43175f19996b6a1a285a4df5b858a8de0e450e5
Hadi Tedi <haditedi@gmail.com>
2024-10-11 06:49:51 +0100

    changed contact

--- app/(thehome)/contact/page.js
+++ app/(thehome)/contact/page.js
@@ -5,7 +5,7 @@ export default function Contact() {
     <section className="flex flex-col items-center justify-center mt-3 mb-3 p-2">
       <div className="w-[250px] h-[350px] md:w-[550px] p-2">
         <Heading src="/call.svg" text="Contact" />
-        <p>For emergency, please contact Hadi at 07825 372243</p>
+        <p>For emergency, please contact Frank at 07468 528068</p>
       </div>
     </section>
   );
